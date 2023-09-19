c865c714ff87044b4996c98b1991334a890346e6
Gayum Sasiri <gayumsasiri.gs@gmail.com>
2023-09-19 19:45:47 +0530

    Text Editor Scene loaded

--- simple-text-editor/src/main/java/lk/ijse/dep11/gayum/AppInitializer.java
+++ simple-text-editor/src/main/java/lk/ijse/dep11/gayum/AppInitializer.java
@@ -1,5 +1,6 @@
 package lk.ijse.dep11.gayum;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,6 +9,7 @@ import javafx.scene.layout.Background;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -21,7 +23,7 @@ public class AppInitializer extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
 
-        AnchorPane root = FXMLLoader.load((getClass().getResource("/view/SplashScene.fxml")));
+        AnchorPane root = FXMLLoader.load((Objects.requireNonNull(getClass().getResource("/view/SplashScene.fxml"))));
         Scene splashScene = new Scene(root);
         Stage stage = new Stage(StageStyle.UNDECORATED);
         stage.setScene(splashScene);
@@ -32,6 +34,30 @@ public class AppInitializer extends Application {
 
         stage.centerOnScreen();
         stage.setResizable(false);
+
         stage.show();
+
+        FadeTransition fadeOutTransition = new FadeTransition(Duration.millis(1000),root);
+        fadeOutTransition.setFromValue(1.0);
+        fadeOutTransition.setToValue(0.0);
+
+        fadeOutTransition.setOnFinished(event -> {
+            stage.close();
+
+            try {
+                AnchorPane textEditorRoot = FXMLLoader.load(getClass().getResource("/view/TextEditorScene.fxml"));
+                Scene textEditorScene = new Scene(textEditorRoot);
+                primaryStage.setScene(textEditorScene);
+                primaryStage.setTitle("* Untitled Document");
+                primaryStage.centerOnScreen();
+                primaryStage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        fadeOutTransition.setDelay(Duration.seconds(2));
+        fadeOutTransition.play();
+
     }
 }
---
+++ simple-text-editor/src/main/java/lk/ijse/dep11/gayum/TextEditorSceneController.java
@@ -0,0 +1,8 @@
+package lk.ijse.dep11.gayum;
+
+import javafx.scene.layout.AnchorPane;
+
+public class TextEditorSceneController {
+
+    public AnchorPane textEditorRoot;
+}
